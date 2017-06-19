ab3afc823c6bb0d2500a16af61379ee009a38afd
EduardoHi <eshidalgo97@gmail.com>
2017-06-18 23:18:42 -0500

    c++ can be executed now + Thread fixes

--- processTest/processTest.java
+++ processTest/processTest.java
@@ -5,59 +5,78 @@ import java.io.*;
 
 public class processTest{
 
-    public static void main(String[] args){
+    static void threadMessage(String message) {
+        String threadName = Thread.currentThread().getName();
+        System.out.format("%s: %s%n",
+                          threadName,
+                          message);
+    }
+
+    public static class OutSideProgram implements Runnable {
+        String type, filename;
+        boolean threadDone = false;
+        BufferedReader reader;
+        BufferedWriter writer;
+        OutSideProgram(String t, String f){
+            type = t;
+            filename = f;
+        }
+
+        public void run() {
+            Thread thisThread = Thread.currentThread();
+
+            try {
+
+                ProcessBuilder pb1 = new ProcessBuilder(type , filename );
+                Process pro1 = pb1.start();
+
+                OutputStream stdin = pro1.getOutputStream ();
+                InputStream stdout = pro1.getInputStream ();
+
+                reader = new BufferedReader(new InputStreamReader(stdout));
+                writer = new BufferedWriter(new OutputStreamWriter(stdin));
+
+                String line = "";
+                while( (line = reader.readLine()) != null){
+                    threadMessage(line);
+                    try{
+                        thisThread.sleep(4000);
+                    } catch( InterruptedException ie){
+                        ie.printStackTrace();
+                    }
+                }
+                reader.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException{
 
         ArrayList<File>sources = getSources();
+        try{
+            compileCpp("hello_world.cpp");
+        } catch (IOException ioe){
+            ioe.printStackTrace();
+        }
 
-        Scanner scan = new Scanner(System.in);
 
-        Thread pythonThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    ProcessBuilder pb1 = new ProcessBuilder("python.exe", "helloworld.py" );
-                    Process pro1 = pb1.start();
-
-                    OutputStream stdin = pro1.getOutputStream ();
-                    InputStream stdout = pro1.getInputStream ();
-
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(stdout));
-                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
-
-                    String line = "";
-                    while( (line = reader.readLine()) != null)
-                        System.out.println(line +" ");
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        } );
-        Thread javaThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    ProcessBuilder pb2 = new ProcessBuilder("java.exe", "HelloWorld" );
-                    Process pro2 = pb2.start();
-
-                    OutputStream stdin = pro2.getOutputStream ();
-                    InputStream stdout = pro2.getInputStream ();
-
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(stdout));
-                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
-
-                    String line = "";
-                    while( (line = reader.readLine()) != null)
-                        System.out.println(line +" ");
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        } );
+        Thread pythonThread = new Thread(new OutSideProgram("python.exe", "helloworld.py") );
+        Thread javaThread = new Thread(new OutSideProgram("java.exe", "HelloWorld") );
+        Thread cppThread = new Thread(new OutSideProgram("hello_world", "") );
 
         pythonThread.start();
         javaThread.start();
+        cppThread.start();
+
+        while (pythonThread.isAlive() || javaThread.isAlive() || cppThread.isAlive() ) {
+            threadMessage("main turn");
+        }
+
+
     }
 
     static ArrayList<File> getSources(){
@@ -73,6 +92,11 @@ public class processTest{
 
     static void compileJava(String filename) throws IOException{
         Process pb = new ProcessBuilder("javac.exe", filename).start();
+    }
+    static void compileCpp(String filename) throws IOException{
+        String outputFile = filename.substring(0,filename.indexOf('.'));
+        System.out.println(outputFile +" "+ filename);
+        Process pb = new ProcessBuilder("g++ ", "-o "+ outputFile + " " + filename).start();
     }
 
 }
